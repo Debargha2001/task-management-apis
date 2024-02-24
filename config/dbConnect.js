bad0f146845f728f652dbe2acc54d3a215ccb1f5
Debargha Dutta <debarghadutta2001@gmail.com>
2024-02-24 05:55:47 +0530

    env added

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,12 +1,7 @@
 const mongoose = require("mongoose");
 
-mongoose
-  .connect(
-    "mongodb+srv://debargha:9UuO50UiebISPNhb@cluster0.99pmzu1.mongodb.net/TaskManagementDB?retryWrites=true&w=majority"
-  )
-  .then((resp) => {
+mongoose.connect(process.env.MONGO_URL).then(resp=> {
     console.log("db connected successfully");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+}).catch(err => {
+    console.log(err)
+})
