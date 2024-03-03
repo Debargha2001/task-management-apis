625203c5924f073e05b0c43112759fc97f87e63a
Debmalya Biswas <viperbale.db@gmail.com>
2024-03-03 09:47:28 +0530

    added cors

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require("express");
 const router = require("./routes/route")
 require("dotenv").config();
 require("./config/dbConnect")
-
+const cors = require("cors")
 const app = express();
 
 app.use(express.json())
-
+app.use(cors())
 const PORT = process.env.PORT || 5000;
 
 app.use("/", router);
